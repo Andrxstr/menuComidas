e703204d34109d444e72f75bf85954b7bfac5633
Andrxstr <andresbarragan0222@gmail.com>
2024-11-05 10:28:37 -0500

    Create Project menuComidas

---
+++ src/main/java/com/menu/menuComidas/MenuComidasApplication.java
@@ -0,0 +1,13 @@
+package com.menu.menuComidas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuComidasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuComidasApplication.class, args);
+	}
+
+}
